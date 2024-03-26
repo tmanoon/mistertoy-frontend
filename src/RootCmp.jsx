861ebce4be869d0de75d93e0cc568a203d6ca67c
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-26 17:46:14 +0200

    Edit root component and add imports

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import '../src/style/main.css'
+
+import { ToyDetails } from './pages/ToyDetails.jsx'
+import { ToyEdit } from './pages/ToyEdit.jsx'
+import { ToyIndex } from './pages/ToyIndex.jsx'
+import { store } from './store/store.js'
+import { AppHeader } from './cmps/AppHeader.jsx'
+import { AppFooter } from './cmps/AppFooter.jsx'
+import { HomePage } from './pages/HomePage.jsx'
 
 export function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Provider store={store}>
+        <Router>
+            <section className="app">
+                <AppHeader />
+                <main className='main-layout'>
+                    <Routes>
+                        <Route element={<HomePage />} path="/" />
+                        {/* <Route element={<AboutUs />} path="/about" /> */}
+                        <Route element={<ToyIndex />} path="/toy" />
+                        <Route element={<ToyEdit />} path="/toy/edit" />
+                        <Route element={<ToyEdit />} path="/toy/edit/:toyId" />
+                        <Route element={<ToyDetails />} path="/toy/:toyId" />
+                        {/* <Route element={<UserDetails />} path="/user/:userId" /> */}
+                    </Routes>
+                </main>
+                <AppFooter />
+            </section>
+        </Router>
+    </Provider>
+
+)
 }
 
