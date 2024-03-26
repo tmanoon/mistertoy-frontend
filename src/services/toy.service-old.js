e84a4641b57ae16abe516042f5a1376fc4094b8f
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-26 15:56:50 +0200

    Create frontend services

---
+++ src/services/toy.service-old.js
@@ -0,0 +1,79 @@
+
+import { storageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
+import { userService } from './user.service.js'
+
+const STORAGE_KEY = 'toyDB'
+const labels = ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle',
+'Outdoor', 'Battery Powered']
+
+export const toyService = {
+    query,
+    getById,
+    save,
+    remove,
+    getEmptyToy,
+    getDefaultFilter
+}
+
+function query(filterBy = {}) {
+    return storageService.query(STORAGE_KEY)
+        .then(toys => {
+            if (!filterBy.txt) filterBy.txt = ''
+            if (!filterBy.maxPrice) filterBy.maxPrice = Infinity
+            const regExp = new RegExp(filterBy.txt, 'i')
+            return toys.filter(toy =>
+                regExp.test(toy.vendor) &&
+                toy.price <= filterBy.maxPrice
+            )
+        })
+}
+
+function getById(toyId) {
+    return storageService.get(STORAGE_KEY, toyId)
+}
+
+function remove(toyId) {
+    // return Promise.reject('Not now!')
+    return storageService.remove(STORAGE_KEY, toyId)
+}
+
+
+function save(toy) {
+    if (toy._id) {
+        return storageService.put(STORAGE_KEY, toy)
+    } else {
+        // when switching to backend - remove the next line
+        toy.owner = userService.getLoggedinUser()
+        return storageService.post(STORAGE_KEY, toy)
+    }
+}
+
+
+function getEmptyToy() {
+    return {
+        id: utilService.makeId(),
+        name: '',
+        labels: '',
+        createdAt: '',
+        inStock: ''
+    }
+}
+
+
+const toy = {
+    _id: 't101',
+    name: 'Talking Doll',
+    price: 123,
+    labels: ['Doll', 'Battery Powered', 'Baby'],
+    createdAt: 1631031801011,
+    inStock: true,
+}
+function getDefaultFilter() {
+    return { txt: '', maxPrice: '' }
+}
+
+// TEST DATA
+// storageService.post(STORAGE_KEY, {vendor: 'Subali Rahok 6', price: 980}).then(x => console.log(x))
+
+
