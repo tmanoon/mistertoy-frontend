8c3df528425a60939590dd304614bd522989c892
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-26 20:52:04 +0200

    Edit toy filter and filtering inside the query function

--- src/cmps/ToyFilter.jsx
+++ src/cmps/ToyFilter.jsx
@@ -10,7 +10,7 @@ export function ToyFilter({ filterBy, onSetFilter }) {
         onSetFilter.current(filterByToEdit)
     }, [filterByToEdit])
 
-   
+
     function handleChange({ target }) {
         let { value, name: field, type } = target
         value = type === 'number' ? +value : value
@@ -18,9 +18,23 @@ export function ToyFilter({ filterBy, onSetFilter }) {
     }
 
     return <section className="contact-filter">
-        <label htmlFor="txt">
-            By text:
+        <label htmlFor="name">
+            By name:
         </label>
-        <input type="text" name="txt" id="txt" value={filterBy.txt} onChange={handleChange} />
+        <input type="text" name="name" id="name" placeholder="Enter the toy's name" value={filterBy.name} onChange={handleChange} />
+        <label>Stock state: </label>
+        <select value={filterByToEdit.stockState} onChange={handleChange} name="inStock">
+            <option value="">Choose</option>
+            <option value="available">In stock</option>
+            <option value="low">Low in stock</option>
+            <option value="gone">Not in stock</option>
+        </select>
+        <label>Sort By:</label>
+        <select value={filterBy.sortBy} onChange={handleChange} name="sortBy">
+            <option value="all">Choose</option>
+            <option value="name">Name</option>
+            <option value="price">Price</option>
+            <option value="createdAt">Creation date</option>
+        </select>
     </section>
 }
--- src/services/toy.service-old.js
+++ src/services/toy.service-old.js
@@ -1,11 +1,13 @@
 
 import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
-import { userService } from './user.service.js'
+// import { userService } from './user.service.js'
 
 const STORAGE_KEY = 'toyDB'
 const labels = ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle',
-'Outdoor', 'Battery Powered']
+    'Outdoor', 'Battery Powered', 'Lego', 'Computer Game']
+
+_createToys()
 
 export const toyService = {
     query,
@@ -19,13 +21,40 @@ export const toyService = {
 function query(filterBy = {}) {
     return storageService.query(STORAGE_KEY)
         .then(toys => {
-            if (!filterBy.txt) filterBy.txt = ''
-            if (!filterBy.maxPrice) filterBy.maxPrice = Infinity
-            const regExp = new RegExp(filterBy.txt, 'i')
-            return toys.filter(toy =>
-                regExp.test(toy.vendor) &&
-                toy.price <= filterBy.maxPrice
-            )
+            let filteredToys = toys
+            if (filterBy.name) {
+                const regex = new RegExp(filterBy.name, 'i')
+                filteredToys = filteredToys.filter(toy => regex.test(toy.name))
+            }
+            if (filterBy.inStock) {
+                if (filterBy.inStock === 'low') {
+                    let lowStockToys = []
+                    let stockOfToys = filteredToys.reduce((acc, currToy) => {
+                        if (currToy.inStock) {
+                            if (acc[currToy.name]) acc[currToy.name]++
+                            else acc[currToy.name] = 1
+                        }
+                        return acc
+                    }, {})
+
+                    console.log(stockOfToys)
+
+                    for (const toy in stockOfToys) {
+                        if (stockOfToys[toy] <= 2 && stockOfToys[toy] > 0) {
+                            lowStockToys.push(filteredToys.find(_toy => _toy.name === toy))
+                        }
+                    }
+                    filteredToys = lowStockToys
+                } else {
+                    const stockStatus = filterBy.inStock === 'available' ? true : false
+                    filteredToys = filteredToys.filter(toy => toy.inStock === stockStatus)
+                }
+            }
+            if (filterBy.sortBy) {
+                if (filterBy.sortBy === 'name') filteredToys = filteredToys.sort((firstToy, secondToy) => firstToy.name.localeCompare(secondToy.name))
+                else filteredToys = filteredToys.sort((firstToy, secondToy) => firstToy[filterBy.sortBy] - secondToy[filterBy.sortBy])
+            }
+            return filteredToys
         })
 }
 
@@ -44,7 +73,7 @@ function save(toy) {
         return storageService.put(STORAGE_KEY, toy)
     } else {
         // when switching to backend - remove the next line
-        toy.owner = userService.getLoggedinUser()
+        // toy.owner = userService.getLoggedinUser()
         return storageService.post(STORAGE_KEY, toy)
     }
 }
@@ -52,7 +81,6 @@ function save(toy) {
 
 function getEmptyToy() {
     return {
-        id: utilService.makeId(),
         name: '',
         labels: '',
         createdAt: '',
@@ -60,17 +88,41 @@ function getEmptyToy() {
     }
 }
 
-
-const toy = {
-    _id: 't101',
-    name: 'Talking Doll',
-    price: 123,
-    labels: ['Doll', 'Battery Powered', 'Baby'],
-    createdAt: 1631031801011,
-    inStock: true,
-}
 function getDefaultFilter() {
-    return { txt: '', maxPrice: '' }
+    return {
+        name: '',
+        inStock: '',
+        sortBy: ''
+    }
+}
+
+function _createToys() {
+    let toys = []
+    toys.push(_createToy('Starry Nights - Lego', 100, [labels[2], labels[8]], new Date(2023, 5, 23).getTime(), false))
+    toys.push(_createToy('Harry Potter - Lego', 100, [labels[2], labels[8]], new Date(2022, 9, 23).getTime(), true))
+    toys.push(_createToy('Harry Potter - Lego', 100, [labels[2], labels[8]], new Date(2022, 9, 23).getTime(), true))
+    toys.push(_createToy('Chess Board - Vanilla JS', 10, [labels[1], labels[2], labels[9]], new Date(2024, 1, 13).getTime(), false))
+    toys.push(_createToy('Miss Bug - Vanilla JS', 10, [labels[2], labels[9]], new Date(2024, 2, 20).getTime(), true))
+    toys.push(_createToy('Robot - RoboHash', 50, [labels[0], labels[2], labels[7]], new Date(2024, 2, 25).getTime(), true))
+    toys.push(_createToy('Robot - RoboHash', 50, [labels[0], labels[2], labels[7]], new Date(2024, 2, 25).getTime(), true))
+    toys.push(_createToy('Robot - RoboHash', 50, [labels[0], labels[2], labels[7]], new Date(2024, 2, 25).getTime(), true))
+    toys.push(_createToy('Jade from Bratz - Bratz', 60, [labels[4], labels[6]], new Date(2023, 6, 29).getTime(), false))
+    toys.push(_createToy('Sarit Hadad - Disney', 60, [labels[4], labels[7]], new Date(2004, 8, 20).getTime(), true))
+    toys.push(_createToy('Checkers for Yom Kippur', 14, [labels[1], labels[2]], new Date(1980, 2, 20).getTime(), true))
+    toys.push(_createToy('Checkers for Yom Kippur', 14, [labels[1], labels[2]], new Date(1980, 2, 20).getTime(), true))
+    toys.push(_createToy('Checkers for Yom Kippur', 14, [labels[1], labels[2]], new Date(1980, 2, 20).getTime(), true))
+    utilService.saveToStorage(STORAGE_KEY, toys)
+}
+
+function _createToy(name, price, labels, createdAt, inStock) {
+    const toy = getEmptyToy()
+    toy._id = utilService.makeId()
+    toy.name = name
+    toy.price = price
+    toy.labels = labels
+    toy.createdAt = createdAt
+    toy.inStock = inStock
+    return toy
 }
 
 // TEST DATA
