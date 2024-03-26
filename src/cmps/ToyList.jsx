648cb3552126d4d2e6de4d3ef5798fa15f5865a6
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-26 20:44:56 +0200

    Add toy list

--- src/cmps/ToyList.jsx
+++ src/cmps/ToyList.jsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { ToyPreview } from './ToyPreview.jsx'
 
-export function ToyList({ onRemoveToy }) {
-	const toys = useSelector(storeState => storeState.toyModule.toys)
+export function ToyList({toys, onRemoveToy }) {
 
-	return <ul className="contact-list">
+	return <ul className="toy-list">
 		{toys.map(toy =>
 			<li key={toy._id}>
 				<Link to={`/toy/${toy._id}`}>
-					<toyPreview toy={toy} />
+					<ToyPreview toy={toy} />
 				</Link>
-				<div className="toy-actions flex space-evenly">
+				<div className="toy-actions">
 					<button className="remove-btn" onClick={() => onRemoveToy(toy._id)}></button>
 					<Link to={`/toy/edit/${toy._id}`}><button className="edit-btn">Edit</button></Link>
 				</div>
