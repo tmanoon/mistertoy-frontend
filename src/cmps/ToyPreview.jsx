cae5c9b33a061560e10d6377edef31c7cf9705fb
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-26 20:44:40 +0200

    Add toy preview

--- src/cmps/ToyPreview.jsx
+++ src/cmps/ToyPreview.jsx
@@ -2,6 +2,13 @@ import { Link } from 'react-router-dom'
 
 export function ToyPreview({ toy }) {
 
+    function transferTimestampToStr(timestamp) {
+        const date = new Date(timestamp)
+        const year = date.getFullYear()
+        const month = date.getMonth() + 1
+        const day = date.getDate()
+        return `${day}/${month}/${year}`
+    }
     return (
         <article className="toy-article flex align-center">
             {/* <img src={toy.img} /> */}
@@ -9,6 +16,7 @@ export function ToyPreview({ toy }) {
                 <h4>{toy.name}</h4>
                 <h6>Price:&nbsp;&nbsp;<span>{toy.price}</span></h6>
                 <h6>Status:&nbsp;&nbsp;<span>{toy.inStock ? 'In stock' : 'Out of stock'}</span></h6>
+                <h6>Created at: {transferTimestampToStr(toy.createdAt)}</h6>
             </div>
         </article>
     )
