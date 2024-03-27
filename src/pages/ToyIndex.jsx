303e3f902e5838769986e0c56ec406899405dc8c
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-27 15:14:02 +0200

    Edit import to right service in toy index cmp

--- src/pages/ToyIndex.jsx
+++ src/pages/ToyIndex.jsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react'
 import { showSuccessMsg, showErrorMsg } from "../services/event-bus.service.js"
 import { ToyFilter } from '../cmps/ToyFilter.jsx'
 import { ToyList } from '../cmps/ToyList.jsx'
-import { loadToys, removeToyOptimistic, saveToy, setFilterBy } from '../store/actions/toy.actions.js'
+import { loadToys, removeToyOptimistic, setFilterBy } from '../store/actions/toy.actions.js'
 // import { ADD_TOY_TO_CART } from '../store/reducers/toy.reducer.js'
 
 export function ToyIndex() {
