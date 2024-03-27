e558e6ce5cc408a3dc2ff04a6879536aa12d80b1
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-27 19:24:40 +0200

    Add link to dashboard in header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -10,6 +10,7 @@ export function AppHeader() {
             <NavLink to="/" >Home</NavLink>
             {/* <NavLink to="/about" >About</NavLink> */}
             <NavLink to="/toy" >Toys</NavLink>
+            <NavLink to="/toy/dashboard" >Dashboard</NavLink>
         </nav>
     </header>
 }
