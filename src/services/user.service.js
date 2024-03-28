b433b625fdad1963b12fc9330f829331a0b84c07
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-28 16:48:16 +0200

    Transorm async functions to async await syntax

---
+++ src/services/user.service.js
@@ -0,0 +1,96 @@
+import { httpService } from './http.service.js'
+
+const BASE_URL = 'auth/'
+const STORAGE_KEY_LOGGEDIN = 'loggedinUser'
+
+export const userService = {
+    login,
+    logout,
+    signup,
+    getById,
+    getLoggedinUser,
+    updateScore,
+    getEmptyCredentials
+}
+
+
+async function login({ username, password }) {
+    try {
+        const user = await httpService.post(BASE_URL + 'login', { username, password })
+        if (user) {
+            return _setLoggedinUser(user)
+        } else {
+            return Promise.reject('Invalid login')
+        }
+    } catch (error) {
+        console.error('Error occurred during login:', error)
+        throw error
+    }
+}
+
+
+async function signup({ username, password, fullname }) {
+    try {
+        const user = { username, password, fullname, score: 10000 }
+        const _user = await httpService.post(BASE_URL + 'signup', user)
+        if (_user) return _setLoggedinUser(_user)
+        else return Promise.reject('Invalid signup')
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
+
+
+async function logout() {
+    try {
+        await httpService.post(BASE_URL + 'logout')
+        sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+async function updateScore(diff) {
+    try {
+        if (getLoggedinUser().score + diff < 0) return Promise.reject('No credit')
+        const user = await httpService.put('/user', { diff })
+        _setLoggedinUser(user)
+        return user.score
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+function getById(userId) {
+    return httpService.get('user/' + userId)
+}
+
+
+function getLoggedinUser() {
+    return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN))
+}
+
+function _setLoggedinUser(user) {
+    const userToSave = { _id: user._id, fullname: user.fullname, score: user.score }
+    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(userToSave))
+    return userToSave
+}
+
+
+function getEmptyCredentials() {
+    return {
+        username: '',
+        password: '',
+        fullname: ''
+    }
+}
+
+
+// Test Data
+// userService.signup({username: 'bobo', password: 'bobo', fullname: 'Bobo McPopo'})
+// userService.login({username: 'bobo', password: 'bobo'})
+
+
+
--- src/services/util.service.js
+++ src/services/util.service.js
@@ -9,7 +9,6 @@ export const utilService = {
 }
 
 function makeId(length = 6) {
-    
     var txt = ''
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
