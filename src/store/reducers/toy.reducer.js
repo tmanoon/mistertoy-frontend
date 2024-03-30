8d22e1799a123436120dc809922a22217ca86dd8
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-30 21:08:54 +0300

    Change way of importing files and move unnecessary cmds to end of file

--- src/store/reducers/toy.reducer.js
+++ src/store/reducers/toy.reducer.js
@@ -1,4 +1,4 @@
-import { toyService } from "../../services/toy.service.js"
+import { toyService } from "./../../services/toy.service.js"
 
 //* Toys
 export const SET_TOYS = 'SET_TOYS'
@@ -7,11 +7,7 @@ export const ADD_TOY = 'ADD_TOY'
 export const UPDATE_TOY = 'UPDATE_TOY'
 export const TOY_UNDO = 'TOY_UNDO'
 
-//* Shopping cart
-// export const TOGGLE_CART_IS_SHOWN = 'TOGGLE_CART_IS_SHOWN'
-// export const ADD_CAR_TO_CART = 'ADD_CAR_TO_CART'
-// export const REMOVE_CAR_FROM_CART = 'REMOVE_CAR_FROM_CART'
-// export const CLEAR_CART = 'CLEAR_CART'
+
 
 export const SET_FILTER_BY = 'SET_FILTER_BY'
 export const SET_IS_LOADING = 'SET_IS_LOADING'
@@ -87,3 +83,8 @@ export function toyReducer(state = initialState, action = {}) {
             return state
     }
 }
+//* Shopping cart
+// export const TOGGLE_CART_IS_SHOWN = 'TOGGLE_CART_IS_SHOWN'
+// export const ADD_CAR_TO_CART = 'ADD_CAR_TO_CART'
+// export const REMOVE_CAR_FROM_CART = 'REMOVE_CAR_FROM_CART'
+// export const CLEAR_CART = 'CLEAR_CART'
