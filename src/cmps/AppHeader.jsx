5c93ba3d009f72f36bc19049f26a371302ca54ff
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-30 16:13:10 +0300

    Add loginsignup cmp to header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,17 +1,19 @@
 import { NavLink } from 'react-router-dom'
-// import { useSelector, useDispatch } from 'react-redux'
+import { LoginSignup } from '../cmps/LoginSignup.jsx'
 
 export function AppHeader() {
 
-    return <header className="header-section flex align-center space-between">
-        <div>Toys</div>
+    return <>
+        <header className="header-section flex align-center space-between">
+            <div>Toys</div>
+            <nav className="flex">
+                <NavLink to="/" >Home</NavLink>
+                <NavLink to="/about" >About</NavLink>
+                <NavLink to="/toy" >Toys</NavLink>
+                <NavLink to="/toy/dashboard" >Dashboard</NavLink>
+            </nav>
+        </header >
 
-        <nav className="flex">
-            <NavLink to="/" >Home</NavLink>
-            <NavLink to="/about" >About</NavLink>
-            <NavLink to="/toy" >Toys</NavLink>
-            <NavLink to="/toy/dashboard" >Dashboard</NavLink>
-
-        </nav>
-    </header>
+        <LoginSignup />
+    </>
 }
