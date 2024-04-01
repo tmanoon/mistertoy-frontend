e42d92c1a7854edeeca6049ff68b00fd56a1b877
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-01 17:58:27 +0300

    Add list of messages in toy details

--- src/pages/ToyDetails.jsx
+++ src/pages/ToyDetails.jsx
@@ -7,6 +7,14 @@ export function ToyDetails() {
     const { toyId } = useParams()
     const navigate = useNavigate()
 
+    function transferTimestampToStr(timestamp) {
+        const date = new Date(timestamp)
+        const year = date.getFullYear()
+        const month = date.getMonth() + 1
+        const day = date.getDate()
+        return `${day}/${month}/${year}`
+    }
+
     useEffect(() => {
         if (toyId) loadToy()
     }, [toyId])
@@ -27,11 +35,20 @@ export function ToyDetails() {
             <h2>Price: ${toy.price}</h2>
             <h2>In stock: {toy.inStock ? 'Yes' : 'No'}</h2>
             <h2>Id: {toy._id}</h2>
+            <h2>Created At: {transferTimestampToStr(toy.createdAt)}</h2>
             {toy.owner && <h2>Owner: {toy.owner}</h2>}
             <h2>Belongs to categories:
                 {toy.labels.length === 1 && toy.labels[0]}
                 {toy.labels.length > 1 && toy.labels.map((label, idx, arr) => <span key={label}> {label} {idx < arr.length - 1 && ','} </span>)}
             </h2>
+            <h2>Toy messages: </h2>
+            {toy.msgs.length && <ul>{toy.msgs.map(msg => 
+            <li key={msg.id}>
+                <h3>" {msg.txt} "</h3>
+                <h5>By: {msg.by.fullname}, id: {msg.by._id}</h5>
+            </li>
+        )}
+        </ul>}
             <Link to={`/toy/edit/${toy._id}`}>Edit</Link> &nbsp;
             <Link to={`/toy`}>Back</Link>
             <p>
