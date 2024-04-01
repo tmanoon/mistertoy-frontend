03d80a934dca2813dfb17fa3072c45084d3e28bc
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-01 22:45:01 +0300

    Add review reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -9,11 +9,13 @@ import {
 import { toyReducer } from "./reducers/toy.reducer.js"
 import { userReducer } from "./reducers/user.reducer.js"
 import { appReducer } from './reducers/app.reducer.js'
+import { reviewReducer } from './reducers/review.reducer.js'
 
 const rootReducer = combineReducers({
     toyModule: toyReducer,
     userModule: userReducer,
-    appModule: appReducer
+    appModule: appReducer,
+    reviewModule: reviewReducer
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
