f80d02ee5dba0911d33684aa5f9c12313e6814dc
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-01 22:45:20 +0300

    Edit user reducer

--- src/store/reducers/user.reducer.js
+++ src/store/reducers/user.reducer.js
@@ -4,7 +4,10 @@ import { userService } from "./../../services/user.service.js"
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 export const CHANGE_BY = 'CHANGE_BY'
-
+export const SET_WATCHED_USER = 'SET_WATCHED_USER'
+export const REMOVE_USER = 'REMOVE_USER'
+export const SET_USERS = 'SET_USERS'
+export const SET_SCORE = 'SET_SCORE'
 
 //* User
 export const SET_USER = 'SET_USER'
@@ -13,7 +16,9 @@ export const SET_USER_SCORE = 'SET_USER_SCORE'
 
 const initialState = {
     count: 105,
-    loggedInUser: userService.getLoggedInUser()
+    loggedInUser: userService.getLoggedInUser(),
+    users: [],
+    watchedUser : null
 }
 
 export function userReducer(state = initialState, action = {}) {
@@ -36,6 +41,22 @@ export function userReducer(state = initialState, action = {}) {
         case SET_USER_SCORE:
             const loggedInUser = { ...state.loggedInUser, score: action.score }
             return { ...state, loggedInUser }
+            case SET_WATCHED_USER:
+                newState = { ...state, watchedUser: action.user }
+                break
+            case REMOVE_USER:
+                newState = {
+                    ...state,
+                    users: state.users.filter(user => user._id !== action.userId)
+                    
+                }
+                break
+            case SET_USERS:
+                newState = { ...state, users: action.users }
+                break
+            case SET_SCORE:
+                newState = { ...state, user: { ...state.user, score: action.score } }
+                break
         default:
             return state;
     }
