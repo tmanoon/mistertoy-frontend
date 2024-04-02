04f3e921ed45c4e2ca1a9a11b143d317f20e91b2
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-03 01:35:49 +0300

    Edit toy list and toy preview

--- src/cmps/ToyList.jsx
+++ src/cmps/ToyList.jsx
@@ -7,14 +7,7 @@ export function ToyList({toys, onRemoveToy }) {
 	return <ul className="toy-list">
 		{toys.map(toy =>
 			<li key={toy._id}>
-				<Link to={`/toy/${toy._id}`}>
-					<ToyPreview toy={toy} />
-				</Link>
-				<div className="toy-actions">
-					<button className="remove-btn" onClick={() => onRemoveToy(toy._id)}></button>
-					<Link to={`/toy/edit/${toy._id}`}><button className="edit-btn">Edit</button></Link>
-					<Link to={`/toy/${toy._id}`}><button className='details-btn'>Details</button></Link>
-				</div>
+					<ToyPreview toy={toy} onRemoveToy={onRemoveToy} />
 			</li>
 		)}
 	</ul>
--- src/cmps/ToyPreview.jsx
+++ src/cmps/ToyPreview.jsx
@@ -1,4 +1,5 @@
-export function ToyPreview({ toy }) {
+import { Link } from 'react-router-dom'
+export function ToyPreview({ toy, onRemoveToy }) {
 
     function transferTimestampToStr(timestamp) {
         const date = new Date(timestamp)
@@ -7,14 +8,20 @@ export function ToyPreview({ toy }) {
         const day = date.getDate()
         return `${day}/${month}/${year}`
     }
-    return (
-        <article className="toy-article flex align-center">
-            {/* <img src={toy.img} /> */}
+    return (//flex column align-center
+        <article className="toy-preview">
+            
             <div>
                 <h4>{toy.name}</h4>
                 <h6>Price:&nbsp;&nbsp;<span>${toy.price}</span></h6>
                 <h6>Status:&nbsp;&nbsp;<span>{toy.inStock ? 'In stock' : 'Out of stock'}</span></h6>
                 {toy.owner && <h6>Owner: {toy.owner}</h6>}
+                <img className="toy-img" src={`https://robohash.org/${toy.name}?set=set4`} title={`image of ${toy.name}`} />
+                <div className="toy-actions">
+                    <button className="remove-btn" onClick={() => onRemoveToy(toy._id)}></button>
+                    <Link to={`/toy/edit/${toy._id}`}><button className="edit-btn">Edit</button></Link>
+                    <Link to={`/toy/${toy._id}`}><button className='details-btn'>Details</button></Link>
+                </div>
             </div>
         </article>
     )
